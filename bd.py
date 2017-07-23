b54ea8d8ec56ff3b8b46035970741278f39486b2
Hung-wei Chuang <hungwei@berkeley.edu>
2017-07-22 17:48:13 -0700

    yolo stock

--- bd.py
+++ bd.py
@@ -1,6 +1,4 @@
 import copy
-import scipy.integrate
-import scipy.special
 
 # Lists correspond to each week. Index 0 = week 1, etc.
 stocks_history = {'KLAC': [96, 93, 92, 97], 'LRCX': [149, 142, 139, 154], 'ADI': [77, 76, 75, 80], 'ACN': [100, 80, 70, 110]};
@@ -10,28 +8,28 @@ cash_history = []
 
 profits = {'hw': 0, 'suhas': 0, 'ralles': 0}
 bdc_portfolio = {'KLAC': 0, 'LRCX': 0, 'ADI': 0, 'ACN': 0, 'cash': 0}
-portfolio_history_extended = []
-portfolio_history = []
-portfolio_history_normalized = []
+history_extended = []
+history = []
+history_norm = []
 portfolio_derivatives = []
 total_deposits = [0]
 
-final_week = 4
+curr_week = 4
 opt = 2
 simplex = 0.30398
 
 def get_portfolio(week_num):
-	return portfolio_history[week_num - 1]
+	return history[week_num - 1]
 
-def update_portfolio_history(port):
-	portfolio_history_extended.append(copy.deepcopy(port))
-	portfolio_history.append(sum_portfolio(port, len(portfolio_history_extended)))
+def update_history(port):
+	history_extended.append(copy.deepcopy(port))
+	history.append(sum_portfolio(port, len(history_extended)))
 
-def get_portfolio_history(week_num):
-	return portfolio_history[week_num - 1]
+def get_history(week_num):
+	return history[week_num - 1]
 
-def get_portfolio_history_extended(week_num):
-	return portfolio_history_extended[week_num - 1]
+def get_history_extended(week_num):
+	return history_extended[week_num - 1]
 
 def get_price(name, week_num):
 	if name == 'cash':
@@ -62,61 +60,61 @@ def invest(amount, week_num):
 def deposit(amount):
 	bdc_portfolio['cash'] += amount
 
-def deposit_all(week_num):
-	total = sum_deposits(week_num)
+def deposit_all(week):
+	total = sum_deposits(week)
 	bdc_portfolio['cash'] += total
 	return total
 
-def sum_deposits(week_num):
-	return sum(deposit_history[x][week_num-1] for x in deposit_history)
+def sum_deposits(week):
+	return sum(deposit_history[x][week-1] for x in deposit_history)
 
-def add_stock(name, amount, week_num):
+def cum_deposits(week):
+	return sum(sum_deposits(x) for x in range(week))
+
+def add_stock(name, amount, week):
 	if name in stocks_history:
 		bdc_portfolio[name] += amount
-		bdc_portfolio['cash'] -= amount * get_price(name, week_num)
+		bdc_portfolio['cash'] -= amount * get_price(name, week)
 
-def sum_portfolio(port, week_num):
-	return sum(port[x] * get_price(x, week_num) for x in port)
+def sum_portfolio(port, week):
+	return sum(port[x] * get_price(x, week) for x in port)
 
-def portfolio_norm(week_num):
-	return portfolio_history_normalized[week_num-1]
+def portfolio_norm(week):
+	return history_norm[week-1]
 
 def back_calculate(total):
 	total_deposits[0] = total
-	for i in range(1, final_week + 1):
-		p = portfolio_history_extended
-		diff = total - sum(sum_deposits(x) for x in range(1, i+1))
-		portfolio_history_normalized.append(diff + portfolio_history[i-1])
+	for i in range(1, curr_week + 1):
+		diff = total - cum_deposits(i)
+		history_norm.append(diff + history[i-1])
 
 def calculate_profits():
-	net_profit = portfolio_history_normalized[-1] - total_deposits[0]
+	net_profit = history_norm[-1] - total_deposits[0]
 	budget = net_profit
 	print("net profits: " + str(net_profit))
-	final = portfolio_history_normalized[-1]
-	for i in reversed(range(1, final_week)):
-		# final = portfolio_norm(i+1)
+	final = history_norm[-1]
+	for i in range(1, curr_week):
 		curr = portfolio_norm(i)
-		diff = (final - curr) / 1
+		diff = (final - curr)
 		for member in profits:
 			profits[member] += diff * get_deposit(member, i)
+
 	whole = sum(profits.values())
 	for member in profits:
-		bessel_func = scipy.integrate.quad(lambda x: scipy.special.jv(opt,x), 0, 4.5)
-		profits[member] = net_profit * profits[member] / whole * (bessel_func[0] - simplex)
+		profits[member] = net_profit * profits[member] / whole
 
 	print(profits)
 
 
 
-
 def run():
 	deposits = 0
-	for week in range(1, final_week + 1):
+	for week in range(1, curr_week + 1):
 		deposits += deposit_all(week)
 		invest(get_cash(), week)
-		update_portfolio_history(bdc_portfolio)
+		update_history(bdc_portfolio)
 	back_calculate(deposits)
-	print(portfolio_history_normalized)
+	print(history_norm)
 	calculate_profits()
 
 
---
+++ stocks/random_stock.py
@@ -0,0 +1,42 @@
+# import csv
+# import random
+
+# stocks = []
+
+# def read():
+# 	with open('all_stocks.csv') as csvfile:
+# 		reader = csv.reader(csvfile)
+# 		for row in reader:
+# 			stocks.append(row)
+
+# def pick_random(sector='default', industry='default'):
+# 	if sector == 'default' and industry =='default':
+# 		return random.choice(stocks)
+# 	else:
+# 		return random.choice([i for i in stocks if i[3] == sector])
+# read()
+# print(pick_random('Finance', 'Major Banks'))
+
+import csv
+import random
+
+stocks = []
+
+with open('all_stocks.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		stocks.append(row)
+
+def pick_random(year='d', sector='d', industry='d'):
+	lst = stocks
+	if year != 'd':
+		lst = list(filter(lambda x: x[2] == year, lst))
+	if sector != 'd':
+		lst = list(filter(lambda x: x[3] == sector, lst))
+	if industry != 'd':
+		lst = list(filter(lambda x: x[4] == industry, lst))
+	return random.choice(lst)
+
+print(pick_random(sector='Finance', industry='Major Banks'))
+
+
