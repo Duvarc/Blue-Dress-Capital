55dcfc29c9269b87f359355650b0fa4a284db80e
Hung-wei Chuang <hungwei@berkeley.edu>
2017-07-22 17:58:25 -0700

    random port

--- stocks/random_stock.py
+++ stocks/random_stock.py
@@ -18,7 +18,15 @@ def pick_random(year='d', sector='d', industry='d'):
 		lst = list(filter(lambda x: x[4] == industry, lst))
 	return random.choice(lst)
 
-print(pick_random(sector='Finance', industry='Major Banks'))
-print(pick_random())
+def random_portfolio(year='d', sector='d', industry='d', size=10):
+	return [pick_random(year, sector, industry) for i in range(size)]
+
+def print_port(year='d', sector='d', industry='d', size=10):
+	for i in random_portfolio(year, sector, industry, size):
+		print(i)
+
+# print(pick_random(sector='Finance', industry='Major Banks'))
+# print(pick_random())
+print_port()
 
 
