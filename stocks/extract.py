b24d62a54e10a44d00cdbc1e27c72b6233e8ed71
Hung-wei Chuang <hungwei@berkeley.edu>
2017-07-22 17:11:33 -0700

    stripped irrelevant info

--- stocks/extract.py
+++ stocks/extract.py
@@ -19,15 +19,22 @@ exchanges = ["nasdaq.csv",
 
 stocks = []
 
-for i in exchanges:
-	with open(i) as csvfile:
-		reader = csv.reader(csvfile)
-		for row in reader:
-			stocks.append(row)
+def read():
+	for i in exchanges:
+		with open(i) as csvfile:
+			reader = csv.reader(csvfile)
+			for row in reader:
+				stocks.append(row)
 
-with open('all_stocks.csv', 'w') as csvfile:
-	writer = csv.writer(csvfile)
-	for row in stocks:
-		writer.writerow(row)
-
+def shorten():
+	for i in range(len(stocks)):
+		stocks[i] = stocks[i][:2] + stocks[i][5:8]
+def write():
+	with open('all_stocks.csv', 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		for row in stocks:
+			writer.writerow(row)
+read()
+shorten()
+write()
 
